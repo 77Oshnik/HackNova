d2855ad9673f27d60843479bf25d0dda6d34b07e
77Oshnik <tiwarioshnikdeep@gmail.com>
2025-02-21 11:26:14 +0530

    setup of frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./config/database");
+
+const app = express();
+
+// Connect Database
+// connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+// app.use(morgan("dev"));
+
+// Routes
+// app.use("/api/users", require("./routes/userRoutes"));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl text-center'>Hello There!</h1>
+      <Button>Click me</Button>
+      <Button>fuck off!!</Button>
+    </>
+  )
+}
+
+export default App
